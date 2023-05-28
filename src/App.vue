5c5f536cd8b54cde2d22367c313a9c82b341b03c
popoola temitope <popoolatopzy@gmail.com>
2023-05-28 03:26:45 +0100

    feature: scaffold vue 3 and vite project

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+  <center><h3>Latest Shoes</h3></center>
+  <div class="container">
+    <div class="card">
+      <img
+        src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80"
+        alt="Product 1"
+      />
+      <h3>Product 1</h3>
+      <p>Description of Product 1</p>
+      <button>Add to Cart</button>
+    </div>
+    <div class="card">
+      <img
+        src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1180&q=80"
+        alt="Product 2"
+      />
+      <h3>Product 2</h3>
+      <p>Description of Product 2</p>
+      <button>Add to Cart</button>
+    </div>
+    <div class="card">
+      <img
+        src="https://images.unsplash.com/photo-1603808033192-082d6919d3e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=830&q=80"
+        alt="Product 3"
+      />
+      <h3>Product 3</h3>
+      <p>Description of Product 3</p>
+      <button>Add to Cart</button>
+    </div>
+    <!-- Add more product cards here -->
+
+    <div class="card">
+      <img
+        src="https://images.unsplash.com/photo-1603808033192-082d6919d3e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=830&q=80"
+        alt="Product 3"
+      />
+      <h3>Product 3</h3>
+      <p>Description of Product 3</p>
+      <button>Add to Cart</button>
+    </div>
+  </div>
+</template>
